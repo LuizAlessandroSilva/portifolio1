c9dd99da7388d5e7b23e8b5dedb489e1bc196bbf
Luiz Alessandro <portifolioluizalessandro@gmail.com>
2024-12-06 18:50:52 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,7 @@
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('btn-mobile')
+
+btnMenu.addEventListener('click',()=>{
+    menu.classList.add('abrir-menu')
+})
